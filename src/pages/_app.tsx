e11b8f6957f22ac7698f3710cc4bccf02b993008
jaewook-jeong <dnrlwo11@gmail.com>
2023-05-11 16:03:47 +0900

    chore: 불필요한 파일 및 코드 제거

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>CLASS가 다르다, 엘리스 아카데미</title>
+        <meta
+          name="description"
+          content="디지털 교육 실습 플랫폼 엘리스 아카데미에서 클래스가 다른 학습을 경험해보세요!"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
