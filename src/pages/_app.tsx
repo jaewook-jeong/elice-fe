eeee7923386fddcc0be147d9ba643c073189e9a2
jaewook-jeong <dnrlwo11@gmail.com>
2023-05-12 08:32:50 +0900

    feat: rq및 errorboundary 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,40 @@
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import { useState } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import ErrorBoundary from '@/features/error/ErrorBoundary';
 import '../styles/global.css';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            useErrorBoundary: true,
+            retry: 1,
+            refetchOnWindowFocus: false,
+          },
+        },
+      }),
+  );
+
   return (
     <>
-      <Head>
-        <title>CLASS가 다르다, 엘리스 아카데미</title>
-        <meta
-          name="description"
-          content="디지털 교육 실습 플랫폼 엘리스 아카데미에서 클래스가 다른 학습을 경험해보세요!"
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Component {...pageProps} />
+      <ErrorBoundary>
+        <QueryClientProvider client={queryClient}>
+          <Head>
+            <title>CLASS가 다르다, 엘리스 아카데미</title>
+            <meta
+              name="description"
+              content="디지털 교육 실습 플랫폼 엘리스 아카데미에서 클래스가 다른 학습을 경험해보세요!"
+            />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
+          <Component {...pageProps} />
+        </QueryClientProvider>
+      </ErrorBoundary>
     </>
   );
 }
