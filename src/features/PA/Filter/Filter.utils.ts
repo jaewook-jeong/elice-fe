19e98105ed41c934bc87a607f8d03c88646dbb0a
jaewook-jeong <dnrlwo11@gmail.com>
2023-05-12 08:31:40 +0900

    chore: 위치변경된 파일 제거

--- src/features/PA/Filter/Filter.utils.ts
+++
@@ -1,6 +0,0 @@
-export const makeQueryArray = <T extends string>(queryValue: T | T[] | undefined): T[] => {
-  if (!queryValue) {
-    return [];
-  }
-  return Array.isArray(queryValue) ? queryValue : [queryValue];
-};
