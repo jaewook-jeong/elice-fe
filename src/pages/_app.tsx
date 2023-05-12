e995e4f3db0aadeb87925e0da79bbc0725ddec07
jaewook-jeong <dnrlwo11@gmail.com>
2023-05-12 11:22:41 +0900

    fix: favicon 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -30,7 +30,7 @@ export default function App({ Component, pageProps }: AppProps) {
               content="디지털 교육 실습 플랫폼 엘리스 아카데미에서 클래스가 다른 학습을 경험해보세요!"
             />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <link rel="icon" href="/favicon.ico" />
+            <link rel="icon" href="/favicon.png" />
           </Head>
           <Component {...pageProps} />
         </QueryClientProvider>
